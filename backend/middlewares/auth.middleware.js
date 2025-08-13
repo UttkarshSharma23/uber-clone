8246c7e503ab93fd13296035a1d53df267e9d5d6
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2025-08-14 00:37:01 +0530

    logout api

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -10,6 +10,21 @@ module.exports.authUser = async (req,res,next)=>{
         return res.status(401).json({ message: "Unauthorized access" });
     }
 
+//NOTE:
+    /*
+     * check blacklist token this is done as the user can share the logout token with other person on other device
+     * so that person can not use the token to access the user profile
+     * This is a simple implementation, in production you might want to use a more robust solution
+     * like storing blacklisted tokens in a database or cache 
+     */ 
+      
+    const isBlacklisted = await userModel.findOne({token : token});
+
+    //check if token is blacklisted
+    if(isBlacklisted){
+        return res.status(401).json({ message: "Unauthorized access" });
+    }
+
     //decode token
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
