fede375332bc9800e0e5ad95bdd309ec0be9d7cd
Uttkarsh Sharma <88883759+UttkarshSharma23@users.noreply.github.com>
2025-08-17 11:33:41 +0530

    fixed bugs (#10)

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,7 +1,7 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const captainModel = require('../models/captain.model');
 
 const captainService = require('../services/captain.service');
-
 
 const {validationResult} = require('express-validator');
 
@@ -25,7 +25,7 @@ module.exports.registerCaptain = async (req, res,next) => {
             { message: 'Captain already exists' }
         );
      }
-
+    //console.log(req.body)
     const hashpassword = await captainModel.hashpassword(password);
 
     // Create a new captain using the service
@@ -40,7 +40,56 @@ module.exports.registerCaptain = async (req, res,next) => {
         vehicleType: vehicle.vehicleType,
     });
 
+
     const token  = captain.generateAuthToken();
     res.status(201).json({ token, captain });
 }
 
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Check for validation errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    // Check if captain exists
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await captain.comparePassword(password, captain.password);
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Generate auth token
+    const token = captain.generateAuthToken();
+    
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+}
+
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain : req.captain });
+}
+
+
+module.exports.logoutCaptain = async (req, res, next) => {
+
+    const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
+
+    await blacklistTokenModel.create({token});
+
+    res.clearCookie('token');
+
+    res.status(200).json({ message: "Logged out successfully" });
+}
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -6,27 +6,40 @@ const { body } = require("express-validator");
 
 
 const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
+// Register route for captain
 router.post('/register', [
-    body('email').isEmail().withMessage('Invalid Email'),
+  body('email').isEmail().withMessage('Invalid Email'),
 
-    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
+  body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
 
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 
-    body('vehicle.color').isLength({ min: 3 }).withMessage('Vehicle color must be at least 3 characters long'),
+  body('vehicle.color').isLength({ min: 3 }).withMessage('Vehicle color must be at least 3 characters long'),
 
-    body('vehicle.plate')
-  .matches(/^[A-Z]{2}\s?\d{2}\s?[A-Z]{1,2}\s?\d{1,4}$/i)
-  .withMessage('Please fill a valid vehicle plate number'),
+  body('vehicle.plate')
+    .matches(/^[A-Z]{2}\s?\d{2}\s?[A-Z]{1,2}\s?\d{1,4}$/i)
+    .withMessage('Please fill a valid vehicle plate number'),
 
-    body('vehicle.capacity').isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
+  body('vehicle.capacity').isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
 
-    body('vehicle.vehicleType').isLength({ min: 3 }).withMessage('Vehicle type must be at least 3 characters long')
+  body('vehicle.vehicleType').isLength({ min: 3 }).withMessage('Vehicle type must be at least 3 characters long')
 ],
 
-        captainController.registerCaptain
+  captainController.registerCaptain
 )
 
+// Login route for captain
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], captainController.loginCaptain);
+
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile);
+
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain);
 
 module.exports = router;
